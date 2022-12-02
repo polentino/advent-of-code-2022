3b93431c3633b38331c732200c9b7bc786f372fd
DIego Casella <polentino911@gmail.com>
2022-12-02 01:41:03 +0100

    order by descending values & tidy up code

--- src/main/scala/io/github/polentino/aoc2022/day01/Ex01.scala
+++ src/main/scala/io/github/polentino/aoc2022/day01/Ex01.scala
@@ -4,14 +4,13 @@ object Ex01 {
 
   def solve(lines: List[String]): Seq[Int] = {
     val (listAcc, acc) = lines
-      .foldLeft((List.empty[List[Int]], List.empty[Int]))({ case ((listAcc, acc), current) =>
+      .foldLeft((List.empty[List[Int]], List.empty[Int])) { case ((listAcc, acc), current) =>
         if (current.isBlank) (listAcc :+ acc, List.empty)
         else (listAcc, acc :+ Integer.parseInt(current))
-      })
+      }
 
     (listAcc :+ acc)
       .map(_.sum)
-      .sorted
-      .reverse
+      .sorted(Ordering.Int.reverse)
   }
 }
