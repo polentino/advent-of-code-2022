623c0f8eabb126a826e5f68ae8a32666008abce9
DIego Casella <polentino911@gmail.com>
2022-12-02 02:04:13 +0100

    added private utility case class to make code more readable

--- src/main/scala/io/github/polentino/aoc2022/day01/Ex01.scala
+++ src/main/scala/io/github/polentino/aoc2022/day01/Ex01.scala
@@ -6,14 +6,29 @@ object Ex01 {
   def findTopThreeMax(lines: List[String]): Int = solve(lines).take(3).sum
 
   private def solve(lines: List[String]): Seq[Int] = {
-    val (listAcc, acc) = lines
-      .foldLeft((List.empty[List[Int]], List.empty[Int])) { case ((listAcc, acc), current) =>
-        if (current.isBlank) (listAcc :+ acc, List.empty)
-        else (listAcc, acc :+ Integer.parseInt(current))
+    lines
+      .foldLeft(Accumulator.empty) { case (accumulator, current) =>
+        if (current.isBlank) accumulator.advance
+        else accumulator.store(Integer.parseInt(current))
       }
+      .sumAndSort
+  }
 
-    (listAcc :+ acc)
-      .map(_.sum)
-      .sorted(Ordering.Int.reverse)
+  private case class Accumulator(pastLines: List[List[Int]], currentLines: List[Int]) {
+
+    def advance: Accumulator =
+      copy(pastLines = pastLines :+ currentLines, currentLines = List.empty)
+
+    def store(value: Int): Accumulator =
+      copy(currentLines = currentLines :+ value)
+
+    def sumAndSort: Seq[Int] =
+      (pastLines :+ currentLines)
+        .map(_.sum)
+        .sorted(Ordering.Int.reverse)
+  }
+
+  private object Accumulator {
+    def empty: Accumulator = Accumulator(Nil, Nil)
   }
 }
