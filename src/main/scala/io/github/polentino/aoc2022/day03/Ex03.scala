a8125922441b29939d35a1612317a60577c55451
DIego Casella <polentino911@gmail.com>
2022-12-03 11:05:44 +0100

    solutions for day 03 withouth the hackish `.store("")`

--- src/main/scala/io/github/polentino/aoc2022/day03/Ex03.scala
+++ src/main/scala/io/github/polentino/aoc2022/day03/Ex03.scala
@@ -7,37 +7,35 @@ object Ex03 {
       val firstCompartment  = line.take(line.length / 2).toCharArray
       val secondCompartment = line.drop(line.length / 2).toCharArray
       firstCompartment.intersect(secondCompartment)
-      .distinct
-      .map(assignScore)
-      .sum
+        .distinct
+        .map(assignScore)
+        .sum
     ).sum
   }
 
   def sumOfGroupedPriorities(lines: List[String]) =
     lines.foldLeft(Accumulator()) { case (acc, string) => acc.store(string) }
-      .store("")
-      .sumOfPriorities
+      .computePriorities
 
   // https://en.wikipedia.org/wiki/ASCII#Printable_characters
   private def assignScore(char: Char) = char.toInt - (if (char.isUpper) 38 else 96)
 
-  private case class Accumulator(sumOfPriorities: Int = 0, currentLines: List[String] = Nil) {
+  private case class Accumulator(pastGroups: List[List[String]] = Nil, currentGroup: List[String] = Nil) {
 
     def store(value: String): Accumulator = {
-      if (currentLines.length == 3) advance(value: String)
-      else copy(currentLines = currentLines :+ value)
+      if (currentGroup.length == 3) copy(pastGroups = pastGroups :+ currentGroup, currentGroup = List(value))
+      else copy(currentGroup = currentGroup :+ value)
     }
 
-    private def advance(value: String): Accumulator = {
-      val firstLine        = currentLines(0).toCharArray
-      val secondLine       = currentLines(1).toCharArray
-      val thirdLine        = currentLines(2).toCharArray
-      val commonCharsScore = (firstLine intersect secondLine intersect thirdLine)
-        .distinct
-        .map(assignScore)
-        .sum
-
-      copy(sumOfPriorities = sumOfPriorities + commonCharsScore, currentLines = List(value))
-    }
+    def computePriorities: Int = (pastGroups :+ currentGroup)
+      .map { group =>
+        val firstLine        = group(0).toCharArray
+        val secondLine       = group(1).toCharArray
+        val thirdLine        = group(2).toCharArray
+        (firstLine intersect secondLine intersect thirdLine)
+          .distinct
+          .map(assignScore)
+          .sum
+      }.sum
   }
 }
