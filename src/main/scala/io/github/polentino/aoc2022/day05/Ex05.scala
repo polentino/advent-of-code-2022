5bb58478bcf738fbe14997528b4c41e7c5c69a85
DIego Casella <polentino911@gmail.com>
2022-12-05 21:58:04 +0100

    solutions for day 05 - do not use mutable state

--- src/main/scala/io/github/polentino/aoc2022/day05/Ex05.scala
+++ src/main/scala/io/github/polentino/aoc2022/day05/Ex05.scala
@@ -1,7 +1,6 @@
 package io.github.polentino.aoc2022.day05
 
 import scala.collection.mutable
-import scala.collection.mutable.ListBuffer
 
 object Ex05 {
   private val charRx = "([A-Z])".r
@@ -10,7 +9,7 @@ object Ex05 {
   def solveSingle(lines: List[String]): String   = solve(lines, _.reverse)
   def solveMultiple(lines: List[String]): String = solve(lines)
 
-  private def solve(lines: List[String], orderingFn: mutable.Seq[Char] => mutable.Seq[Char] = identity) = {
+  private def solve(lines: List[String], orderingFn: Seq[Char] => Seq[Char] = identity) = {
     // split the whole lines into the part where the initial state is defined, and the one where the moves are
     val stateLines = lines.takeWhile(_.nonEmpty)
     val movesLines = lines.dropWhile(_.nonEmpty).tail
@@ -21,32 +20,48 @@ object Ex05 {
     val stackPositions = Range.inclusive(1, (stackNr - 1) * 4 + 1, 4).toList
     val stacks         = stateLines.dropRight(1)
 
-    // build the initial state: create as many empty list as the number of stacks
-    val emptyStackState: mutable.Seq[mutable.Seq[Char]] =
-      mutable.ListBuffer.fill(stackNr)(mutable.ListBuffer.empty[Char])
     // build the stack state by filling the corresponding list, if there is a valid A-Z character
-    val stackState                                      = stacks.foldLeft(emptyStackState) { case (state, line) =>
-      stackPositions.zipWithIndex.foreach { (index, stackIndex) =>
+    val initialStackState = stacks.foldLeft(StackState(stackNr)) { case (state, line) =>
+      stackPositions.zipWithIndex.foldLeft(state) { case (state, (index, stackIndex)) =>
         line(index) match {
-          case charRx(value) => state(stackIndex) = state(stackIndex) :+ value
+          case charRx(value) => state.addElement(value, stackIndex)
           case _             => state
         }
       }
-      state
     }
 
     // parse the moves and apply them to the stack state
-    movesLines.foreach {
-      case moveRx(qty, f, t) =>
-        val quantity             = Integer.parseInt(qty)
-        val from                 = Integer.parseInt(f) - 1
-        val to                   = Integer.parseInt(t) - 1
-        val (removed, remaining) = stackState(from).splitAt(quantity)
-        stackState(from) = remaining
-        stackState(to) = orderingFn(removed) ++ stackState(to)
+    val finalStackState = movesLines.foldLeft(initialStackState) { case (state, line) =>
+      line match {
+        case moveRx(qty, f, t) =>
+          val quantity = Integer.parseInt(qty)
+          val from     = Integer.parseInt(f) - 1
+          val to       = Integer.parseInt(t) - 1
+          state.move(quantity, from, to, orderingFn)
+      }
     }
 
-    // combine the result
-    stackState.map(_.head).mkString
+    finalStackState.result
   }
+
+  private final case class StackState(state: Seq[Seq[Char]] = Nil) {
+
+    def addElement(ch: Char, position: Int) =
+      copy(state = state.updated(position, state(position) :+ ch))
+
+    def move(quantity: Int, from: Int, to: Int, orderingFn: Seq[Char] => Seq[Char]) = {
+      val (removed, remaining) = state(from).splitAt(quantity)
+      val updated              = state
+        .updated(from, remaining)
+        .updated(to, orderingFn(removed) ++ state(to))
+      copy(state = updated)
+    }
+
+    def result = state.map(_.head).mkString
+  }
+
+  private object StackState {
+    def apply(size: Int): StackState = StackState(Seq.fill(size)(Seq.empty[Char]))
+  }
+
 }
